7430482a2cf7e31527c97ead3e36b52599326ef8
ShadowsKing007 <114609169+ShadowsKing007@users.noreply.github.com>
2022-09-28 20:24:09 +0200

    Primera subida

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const toggleMenu = () => {
+  document.getElementById("menu-mob").classList.toggle("menu-open");
+  document.getElementById("burger").classList.toggle("is-active");
+  document.getElementById("darkbg").classList.toggle("visible");
+  document.getElementById("logo").classList.toggle("logo2");
+}
